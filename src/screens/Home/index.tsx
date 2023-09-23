f845df8c500a41fc22c6ed2d0bcbae9d3fd1e1a3
guilherme.fonseca2021 <gsbloogs194@gmail.com>
2023-09-22 23:58:08 -0300

    mutabilidade

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,35 @@
-import { Text, View, TextInput, TouchableOpacity, FlatList } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from "react-native";
 import { styles } from './styles'
 import { Participant } from "../../components/Participant";
+import { useState } from "react";
 
 export default function Home() {
-    const participants = ['Guilherme', 'Kallyel', 'Diego', 'Rodrigo', 'Jessica', 'Grazi', 'Emilly', 'Fernanda']
+    const [participants, setParticipants]= useState<string[]>([])
+    const [participantName, setParticiantName] = useState('')
 
     function handleAddPerson() {
-        console.log('voce clicou no botao adicionar!')
+        if(participants.includes(participantName)) {
+            return Alert.alert('Participante existe', 'ja existe um na lista como esse nome')
+        }
+
+        // participants.push('Ana');
+        // setParticipants([...participants, 'Ana'])
+        // ou
+        setParticipants(prevState => [...prevState, participantName])
     }
 
     function handleParticipantRemove(name: string) {
-        console.log(`clico em remover ${name}`)
+        // 1 titulo 2 mensagem 3 butoes
+        Alert.alert(`Remover`, `Remover o participante ${name}?`, [
+            {
+                text: 'sim',
+                onPress: () => Alert.alert('Deletado!')
+            },
+            {
+                text: 'nao',
+                style: 'cancel'
+            }
+        ])
     }
     //view - div
     return (
@@ -22,6 +41,7 @@ export default function Home() {
                 <TextInput style={styles.input} 
                     placeholder="Nome do participante"
                     placeholderTextColor="#6b6b6b"
+                    onChangeText={text => setParticiantName(text)}
                     // keyboardType="email-address"
                 />
 
@@ -41,6 +61,11 @@ export default function Home() {
                     />
                 )}
                 showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => (
+                    <Text style={styles.listEmptyText}>
+                        Ninguem chegou no evento ainda adiciona participantes a sua lista a sua lista de presenca
+                    </Text>
+                )}
             />
         </View>
     );
